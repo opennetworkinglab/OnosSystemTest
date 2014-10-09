4a5c4475800e63078fba6ed028fb44b595313243
andrewonlab <andrew@onlab.us>
2014-10-09 12:11:41 -0400

    Test Case for ONOSNextTest

---
+++ TestON/tests/ONOSNextTest/ONOSNextTest.py
@@ -0,0 +1,21 @@
+
+#Testing the basic functionality of ONOS Next
+#For sanity and driver functionality excercises only.
+
+class ONOSNextTest:
+    def __init__(self):
+        self.default = ''
+
+    def CASE1(self, main):
+        '''
+        Startup sequence:
+        git pull
+        mvn clean install
+        onos-package
+        cell <name>
+        onos-verify-cell
+        onos-install -f
+        onos-wait-for-start
+        '''
+
+

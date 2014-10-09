084307e47c6354c2b868258540e6532e7002eed4
andrewonlab <andrew@onlab.us>
2014-10-09 14:06:09 -0400

    Added environment parameter and incorporated set_cell function

--- TestON/tests/ONOSNextTest/ONOSNextTest.py
+++ TestON/tests/ONOSNextTest/ONOSNextTest.py
@@ -22,14 +22,15 @@ class ONOSNextTest:
         onos-wait-for-start
         '''
         
-        
-        #install_result = main.ONOSbench.clean_install()
-        install_result = main.TRUE
+        cell_name = main.params['ENV']['cellName']
+        ONOS1_ip = main.params['CTRL']['ip1']
+
+        install_result = main.ONOSbench.clean_install()
         package_result = main.ONOSbench.onos_package()
+        cell_result = main.ONOSbench.set_cell(cell_name)
 
-
-
-        case1_result = (install_result and package_result)
+        case1_result = (install_result and package_result and\
+                cell_result)
         utilities.assert_equals(expect=main.TRUE, actual=case1_result,
                 onpass="Test startup successful",
                 onfail="Test startup NOT successful")

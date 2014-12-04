797ebd741e4b1eea1a7d2407cdc539697f7ebada
pingping-lin <pingping@onlab.us>
2014-12-04 14:43:26 -0800

    delete debug code

--- TestON/drivers/common/cli/quaggaclidriver.py
+++ TestON/drivers/common/cli/quaggaclidriver.py
@@ -22,15 +22,11 @@ class QuaggaCliDriver(CLI):
         self.name = self.options['name']
         # self.handle = super(QuaggaCliDriver,self).connect(user_name = self.user_name, ip_address = self.ip_address,port = self.port, pwd = self.pwd)
         self.handle = super(QuaggaCliDriver, self).connect(user_name=self.user_name, ip_address="1.1.1.1", port=self.port, pwd=self.pwd)
-        main.log.info("quagga handle" + str(self.handle) )
-        main.log.info("where is this timeout from?")
         main.log.info("connect parameters:" + str(self.user_name) + ";" + str(self.ip_address) + ";" + str(self.port) + ";" + str(self.pwd))
 
         if self.handle:
             #self.handle.expect("",timeout=10)
             #self.handle.expect("\$",timeout=10)
-            #main.log.info(self.handle.before)
-            #main.log.info(self.handle.after)
             self.handle.sendline("telnet localhost 2605")
             #self.handle.expect("Password:", timeout=5)
             self.handle.expect("Password:")

685359e23dabc16b57be59df3414d0035793d5b8
Jon Hall <jhall@onlab.us>
2015-08-28 11:11:47 -0700

    Clean up extra processes from using multi-ovsdb
    Change-Id: Ib58be5f36af0001a7811a2c69035756baad0e6f0

--- TestON/bin/cleanup.sh
+++ TestON/bin/cleanup.sh
@@ -1,6 +1,7 @@
-#!/bin/bash 
+#!/bin/bash
 # This script will kill any TestON, ssh, and Mininet sessions that are running.
 sudo kill -9 `ps -ef | grep "./cli.py" | grep -v grep | awk '{print $2}'`
 sudo kill -9 `ps -ef | grep "bin/teston" | grep -v grep | awk '{print $2}'`
 sudo kill -9 `ps -ef | grep "ssh -X" | grep -v grep | awk '{print $2}'`
 sudo mn -c
+sudo pkill -f mn.pid

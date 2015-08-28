e019baa00657209bc04f448977ef9609ad9a798c
Jon Hall <jhall@onlab.us>
2015-08-27 17:31:10 -0700

    Adjust timeout
    Change-Id: Ice38430e726bceebdd77cbfff077b98b901ffe6b

--- TestON/tests/SCPFmaxIntents/Dependency/rerouteTopo.py
+++ TestON/tests/SCPFmaxIntents/Dependency/rerouteTopo.py
@@ -15,27 +15,27 @@ from mininet.node import ( UserSwitch, OVSSwitch, IVSSwitch )
 
 class MyTopo( Topo ):
 
-	def __init__( self ):
-		# Initialize topology
-		Topo.__init__( self )
+    def __init__( self ):
+        # Initialize topology
+        Topo.__init__( self )
 
-                host1 = self.addHost('h1', ip='10.1.0.1/24')
-                host2 = self.addHost('h2', ip='10.1.0.2/24')
-                host3 = self.addHost('h3', ip='10.1.0.3/24')
+        host1 = self.addHost('h1', ip='10.1.0.1/24')
+        host2 = self.addHost('h2', ip='10.1.0.2/24')
+        host3 = self.addHost('h3', ip='10.1.0.3/24')
 
-		s1 = self.addSwitch( 's1' )
-		s2 = self.addSwitch( 's2' )
-		s3 = self.addSwitch( 's3' )
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
 
-                self.addLink(s1, host1)
-                self.addLink(s2, host2)
-                self.addLink(s3, host3)
+        self.addLink(s1, host1)
+        self.addLink(s2, host2)
+        self.addLink(s3, host3)
 
-		self.addLink(s1,s2)
-		self.addLink(s1,s3)
-		self.addLink(s2,s3)
+        self.addLink(s1,s2)
+        self.addLink(s1,s3)
+        self.addLink(s2,s3)
 
-		topos = { 'mytopo': ( lambda: MyTopo() ) }
+        topos = { 'mytopo': ( lambda: MyTopo() ) }
 
 # HERE THE CODE DEFINITION OF THE TOPOLOGY ENDS
 

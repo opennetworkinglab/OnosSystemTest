90dfe2f361133f22af5f7bd1c122e2a7b1d73564
pingping-lin <pingping@onlab.us>
2015-09-28 10:57:42 -0700

    add sdn-ip relative clean up command lines
    Change-Id: I75ee6f4e3ad19b83d7f48eda4eafb83632fed775

--- TestON/bin/cleanup.sh
+++ TestON/bin/cleanup.sh
@@ -5,3 +5,6 @@ sudo kill -9 `ps -ef | grep "bin/teston" | grep -v grep | awk '{print $2}'`
 sudo kill -9 `ps -ef | grep "ssh -X" | grep -v grep | awk '{print $2}'`
 sudo mn -c
 sudo pkill -f mn.pid
+sudo pkill bgpd
+sudo pkill zebra
+sudo kill -9 `ps -ef | grep "bird" | grep -v grep | awk '{print $2}'`

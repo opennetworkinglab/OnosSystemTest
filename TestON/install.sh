6268e94ae26dbb220f5af882b8f1309414c498f9
Jon Hall <jhall@ciena.com>
2016-06-22 16:02:06 -0700

    Add python3-requests to install script
    This is needed for a script used by some performance tests
    
    Change-Id: I9e06dbe98b816d8dd3ebd41571113bda98066b0e

--- TestON/install.sh
+++ TestON/install.sh
@@ -63,10 +63,10 @@ fi
 echo "Installing TestON dependencies"
 if [ "$DIST" = "Fedora" ]; then
     # Fedora may have vlan enabled by default. Still need to confirm and update later
-    $install python-pip build-essential python-dev pep8 arping
+    $install python-pip build-essential python-dev pep8 arping python3-requests
     $pipinstall pexpect==3.2 configobj==4.7.2 numpy
 else
-    $install python-pip build-essential python-dev pep8 vlan arping
+    $install python-pip build-essential python-dev pep8 vlan arping python3-requests
     $pipinstall pexpect==3.2 configobj==4.7.2 numpy
 fi
 

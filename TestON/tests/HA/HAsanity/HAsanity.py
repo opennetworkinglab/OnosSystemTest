3bf9b609868a6c588aedbfa662a25bf139796b8d
Jon Hall <jhall@ciena.com>
2016-09-26 11:36:41 -0700

    Update graph indexes for 1.7
    Change-Id: If255e550615f219959647072c2cf0e333d15a9d6

--- TestON/tests/HA/HAsanity/HAsanity.py
+++ TestON/tests/HA/HAsanity/HAsanity.py
@@ -181,7 +181,7 @@ class HAsanity:
         #       index = The number of the graph under plot name
         job = "HAsanity"
         plotName = "Plot-HA"
-        index = "2"
+        index = "1"
         graphs = '<ac:structured-macro ac:name="html">\n'
         graphs += '<ac:plain-text-body><![CDATA[\n'
         graphs += '<iframe src="https://onos-jenkins.onlab.us/job/' + job +\

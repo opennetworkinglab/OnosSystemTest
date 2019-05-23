a37920be4e3f4dc1e8240e477f508cb4f431c9e1
Jeremy Ronquillo <j_ronquillo@u.pacific.edu>
2019-05-23 14:34:25 -0700

    [ONOS-7988]: Refactor Branches used in Jenkins TestON Pipelines
    Change-Id: I5801715e2af8315809130c2d0fd262b304f949f4

--- TestON/JenkinsFile/dependencies/JenkinsTestONTests.groovy
+++ TestON/JenkinsFile/dependencies/JenkinsTestONTests.groovy
@@ -21,22 +21,32 @@
 
 allTests = [:]
 schedules = [:]
+branches = [:]
 
 def init(){
     def tests_buffer = readTrusted( "TestON/JenkinsFile/dependencies/tests.json" )
     def schedules_buffer = readTrusted( "TestON/JenkinsFile/dependencies/schedule.json" )
+    def branches_buffer = readTrusted( "TestON/JenkinsFile/dependencies/branches.json" )
     allTests = readJSON text: tests_buffer
     schedules = readJSON text: schedules_buffer
+    branches = readJSON text: branches_buffer
 }
 
+// returns the entire set of TestON tests from the json file
 def getAllTests(){
     return allTests
 }
 
+// returns the entire set of schedules from the json file
 def getSchedules(){
     return schedules
 }
 
+def getAllBranches(){
+    return branches
+}
+
+// given a test category ("FUNC", "HA", etc.), returns all tests associated with that category
 def getTestsFromCategory( category, tests=[:] ){
     result = [:]
     if ( tests == [:] ){
@@ -50,18 +60,24 @@ def getTestsFromCategory( category, tests=[:] ){
     return result
 }
 
-def getTestsFromDay( day, branch, tests=[:] ){
-    result = [:]
-    if ( tests == [:] ){
-        tests = allTests
-    }
+// given a day, returns schedules that contain that day
+def getValidSchedules( day ){
     validSchedules = []
     for ( String key in schedules.keySet() ){
         if ( schedules[ key ].contains( day ) ){
             validSchedules += key
         }
     }
-    echo validSchedules.toString()
+    return validSchedules
+}
+
+// given a day and branch, returns all tests that run on the given day on the given branch
+def getTestsFromDay( day, branch, tests=[:] ){
+    result = [:]
+    if ( tests == [:] ){
+        tests = allTests
+    }
+    validSchedules = getValidSchedules( day )
     for ( String key in tests.keySet() ){
         schedule = tests[ key ][ "schedule" ][ branch ]
         if ( validSchedules.contains( schedule ) ){
@@ -71,6 +87,26 @@ def getTestsFromDay( day, branch, tests=[:] ){
     return result
 }
 
+// given a day, returns all branches that are run on that day
+def getBranchesFromDay( day, tests=[:] ){
+    result = []
+    if ( tests == [:] ){
+        tests = allTests
+    }
+    validSchedules = getValidSchedules( day )
+
+    for ( String key in tests.keySet() ){
+        for ( String branch in tests[ key ][ "schedule" ].keySet() ){
+            sch = tests[ key ][ "schedule" ][ branch ]
+            if ( validSchedules.contains( sch ) && !result.contains( sch ) ){
+                result += convertBranchCodeToBranch( branch )
+            }
+        }
+    }
+    return result
+}
+
+// given a nodeLabel ("vm", "bm", etc.), returns all tests that run on that node.
 def getTestsFromNodeLabel( nodeLabel, tests=[:] ){
     if ( tests == [:] ){
         tests = allTests
@@ -82,6 +118,7 @@ def getTestsFromNodeLabel( nodeLabel, tests=[:] ){
     }
 }
 
+// returns the test list as a string
 def getTestListAsString( tests ){
     result = ""
     for ( String test in tests.keySet() ){
@@ -90,12 +127,25 @@ def getTestListAsString( tests ){
     return result[ 0..-2 ]
 }
 
+// returns the schedule for a given test
 def getTestSchedule( test ){
     return allTests[ test ][ "schedule" ]
 }
 
+// returns a list of days from the given schedule
 def convertScheduleKeyToDays( sch ){
     return schedules[ sch ]
+}
+
+def convertBranchCodeToBranch( branch_code, withPrefix=true ){
+    for ( String branch_type in branches.keySet() ){
+        for ( String b in branches[ branch_type ].keySet() ){
+            if ( branch_code == b ){
+                return withPrefix ? ( "onos-" + branches[ branch_type ][ b ] ) : branches[ branch_type ][ b ]
+            }
+        }
+    }
+    return branch_code
 }
 
 return this

953e2bb9b62dc550b97cf1efda526300baa307d6
Jon Hall <jhall@ciena.com>
2020-07-30 11:42:22 -0700

    Cleanup any leftover docker containers
    Change-Id: If403fe1d4be21b4621058134bf2ed5083140e382
    (cherry picked from commit cc166706d88b89e10186df6d939690daa6d6f3ac)

--- TestON/tests/USECASE/SegmentRouting/dependencies/Testcaselib.py
+++ TestON/tests/USECASE/SegmentRouting/dependencies/Testcaselib.py
@@ -1594,6 +1594,10 @@ class Testcaselib:
             handle.sendline( "sudo chmod -R a+rwx %s" % ( confDir ) )
             handle.expect( main.Mininet1.prompt )
             main.log.debug( handle.before + handle.after )
+            # Stop any leftover container
+            handle.sendline( "docker stop trellis_mininet" )
+            handle.expect( main.Mininet1.bashPrompt )
+            main.log.debug( handle.before )
             # Start docker container
             handle.sendline( "docker run --name trellis_mininet %s %s" % ( main.params[ 'MN_DOCKER' ][ 'args' ], main.params[ 'MN_DOCKER' ][ 'name' ] ) )
             handle.expect( main.Mininet1.bashPrompt )

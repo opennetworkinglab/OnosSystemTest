e734b7fb2d2c9638faffabf6bf960eef8a15f73c
Daniele Moro <daniele@opennetworking.org>
2021-09-07 17:53:20 +0200

    Add p4runtime shell and trex dependencies on Docker environment
    Change-Id: Ic80a6cfe77d49c12890dc80e789492142a9716b3

---
+++ TestON/docker/fs/home/jenkins/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,7 @@
+# Configure ipython to not use colors, helps the P4RuntimeCliDriver in getting
+# cleaner output
+# More info about configuration in: https://ipython.readthedocs.io/en/stable/config/options/terminal.html
+c.InteractiveShell.color_info = False
+c.InteractiveShell.colors = 'NoColor'
+c.TerminalInteractiveShell.color_info = False
+c.TerminalInteractiveShell.colors = 'NoColor'
